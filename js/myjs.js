2aaf26f4dd07d33f92b858213f30faed623ec0c7
pranati05 <pranati05@gmail.com>
2016-06-07 09:53:03 -0700

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+// Display Popup
+
+
+function div_show(){
+	document.getElementById('div_popup').style.display="block";
+}
+
+// Hide Popup
+
+
+
+function div_hide(){
+	document.getElementById('div_popup').style.display="none";
+		
+	
+}
